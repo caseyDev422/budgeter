fb415adbd2dd30194a4eff39053460cfe4cabd0c
Sean Casey <seancasey076@gmail.com>
2022-12-01 18:07:02 -0600

    added styling for form

--- client/budgeter/src/components/BillForm.tsx
+++ client/budgeter/src/components/BillForm.tsx
@@ -13,44 +13,66 @@ function BillForm(props: any) {
         formValues.picked === 'Yes' ? formValues.hasAutoDraft = true : formValues.hasAutoDraft = false;
         console.log('formValues', formValues);
     }
-    return(
-        <div>
-            <Dialog open={props.isOpen} aria-labelledby='dialog-title' aria-describedby='dialog-description'>
-                <DialogTitle>Add a Bill</DialogTitle>
-                <DialogContent>
-                    <Formik initialValues={{
-                        billName: '',
-                        amount: '',
-                        hasAutoDraft: false,
-                        picked: ''
-                    }} onSubmit={(values: Bill) => handleFormSubmit(values)}>
-                        {({values, handleChange}) => (
-                            <Form>
-                                <TextField name='billName' label="Bill name" value={values.billName} onChange={handleChange}/>
-                                <TextField name='amount' label="Amount" value={values.amount} onChange={handleChange}/>
-                                <div id="my-radio-group">Picked</div>
-                                    <div role="group" aria-labelledby="my-radio-group">
-                                    <label>
-                                    <Field type="radio" name="picked" value="Yes"/>
-                                    Yes
-                                    </label>
-                                    <label>
-                                    <Field type="radio" name="picked" value="No"/>
-                                    No
-                                    </label>
-                                </div>
-                               <DialogActions>
-                                <Button onClick={() => props.handleOpen(false)}>Cancel</Button>
-                                <Button type='submit'>Submit</Button>
-                               </DialogActions> 
-                            </Form>
-                        )}
-                    </Formik>
-                </DialogContent>
-                
-            </Dialog>
-        </div>
-    )
+    return (
+      <div>
+        <Dialog
+          open={props.isOpen}
+          aria-labelledby="dialog-title"
+          aria-describedby="dialog-description"
+        >
+          <DialogTitle>Add a Bill</DialogTitle>
+          <DialogContent>
+            <Formik
+              initialValues={{
+                billName: "",
+                amount: "",
+                hasAutoDraft: false,
+                picked: "",
+              }}
+              onSubmit={(values: Bill) => handleFormSubmit(values)}
+            >
+              {({ values, handleChange }) => (
+                <Form>
+                  <div className='billForm'>
+                    <TextField
+                        className='billForm-item'
+                        name="billName"
+                        label="Bill name"
+                        value={values.billName}
+                        onChange={handleChange}
+                    />
+                    <TextField
+                        className='billForm-item'
+                        name="amount"
+                        label="Amount"
+                        value={values.amount}
+                        onChange={handleChange}
+                    />
+                    <div id="my-radio-group">Auto draft?</div>
+                    <div role="group" aria-labelledby="my-radio-group">
+                      <label>
+                        <Field type="radio" name="picked" value="Yes" />
+                        Yes
+                      </label>
+                      <label style={{paddingLeft: '20px'}}>
+                        <Field type="radio" name="picked" value="No" />
+                        No
+                      </label>
+                    </div>
+                    <DialogActions>
+                      <Button onClick={() => props.handleOpen(false)}>
+                        Cancel
+                      </Button>
+                      <Button type="submit">Submit</Button>
+                    </DialogActions>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
 }
 
 export default BillForm;
